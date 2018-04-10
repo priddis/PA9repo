7be8af93573d4edb20c7dadec3cb377c49f47fcd
micah-priddis <37024281+micah-priddis@users.noreply.github.com>
2018-04-09 21:55:57 -0700

    main.cpp
    This is the basic code from Andy. The only change I made was to set the framerate.

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include <SFML/Graphics.hpp>
+
+int main()
+{
+	sf::RenderWindow window(sf::VideoMode(800, 800), "SFML works!");
+	sf::CircleShape shape(100.f);
+	shape.setFillColor(sf::Color::Green);
+
+	window.setFramerateLimit(60);
+
+	while (window.isOpen())
+	{
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+
+		window.clear();
+		window.draw(shape);
+		window.display();
+	}
+
+	return 0;
+}
